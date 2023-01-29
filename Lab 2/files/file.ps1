b2fa13cbd386e3fbd8305f6cb0cdf59b17fe4549
vonderchild <saadjaved3@yahoo.com>
2023-01-29 22:38:44 +0500

    add files for lab 2

---
+++ Lab 2/files/file.ps1
@@ -0,0 +1,3 @@
+$data  ="SGVsbG8sIHVzZSBmbGFne2V2M250X2wwZ3NfZjByX3RoM193MW59IGFzIHRoZSBhbnN3ZXIgdG8gdGhlIG9yaWdpbmFsIHF1ZXN0aW9uLg=="
+$flag = [System.Text.Encoding]::ASCII.GetString([System.Convert]::FromBase64String($data))
+Write-Output $flag
